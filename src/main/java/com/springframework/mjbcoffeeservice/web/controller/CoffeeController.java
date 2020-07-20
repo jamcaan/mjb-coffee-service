732f0a4f6d4d6e1327c7137f7c3f4a476372960a
jamcaan <jawideggan@gmail.com>
2020-07-20 15:58:44 -0400

    added coffee service controller. Closes #3

---
+++ src/main/java/com/springframework/mjbcoffeeservice/web/controller/CoffeeController.java
@@ -0,0 +1,40 @@
+package com.springframework.mjbcoffeeservice.web.controller;
+
+import com.springframework.mjbcoffeeservice.web.model.CoffeeDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+/**
+ * Created by Mohamed Jama 7/20/20
+ */
+@RequestMapping("/api/v1/coffee")
+@RestController
+public class CoffeeController {
+
+    @GetMapping("/{coffeeId}")
+    public ResponseEntity<CoffeeDto> getCoffeeById(@PathVariable ("coffeeId") UUID coffeeId){
+        //todo impl
+        return new ResponseEntity<>(CoffeeDto.builder().build(), HttpStatus.OK);
+
+    }
+
+    @PostMapping
+    public ResponseEntity saveCoffee(@RequestBody CoffeeDto coffeeDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{coffeeId}")
+    public ResponseEntity updateCoffeeById (@PathVariable("coffeeId") UUID coffeeId, @RequestBody CoffeeDto coffeeDto){
+        //todo impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+
+
+
+}
