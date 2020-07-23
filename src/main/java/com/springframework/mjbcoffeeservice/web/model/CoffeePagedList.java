5bfdf4bd44ec191ee15abb7ee57fa623c95c025d
jamcaan <jawideggan@gmail.com>
2020-07-23 16:46:27 -0400

    Added Jackson Json Creator for Paged Objects. closes #20

--- src/main/java/com/springframework/mjbcoffeeservice/web/model/CoffeePagedList.java
+++ src/main/java/com/springframework/mjbcoffeeservice/web/model/CoffeePagedList.java
@@ -1,7 +1,11 @@
 package com.springframework.mjbcoffeeservice.web.model;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -10,11 +14,32 @@ import java.util.List;
  * Created by Mohamed Jama
  */
 public class CoffeePagedList extends PageImpl<CoffeeDto> {
-    public CoffeePagedList(List<CoffeeDto> content, Pageable pageable, long total) {
+
+    static final long serialVersionUID = 1114715135625836949L;
+
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public CoffeePagedList(@JsonProperty("content") List<CoffeeDto> content,
+                           @JsonProperty("number") int number,
+                           @JsonProperty("size") int size,
+                           @JsonProperty("totalElements") Long totalElements,
+                           @JsonProperty("pageable") JsonNode pageable,
+                           @JsonProperty("last") boolean last,
+                           @JsonProperty("totalPages") int totalPages,
+                           @JsonProperty("sort") JsonNode sort,
+                           @JsonProperty("first") boolean first,
+                           @JsonProperty("numberOfElements") int numberOfElements ){
+
+        super(content, PageRequest.of(number, size), totalElements);
+    }
+
+   public CoffeePagedList (List<CoffeeDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
     }
 
-    public CoffeePagedList(List<CoffeeDto> content) {
+    public CoffeePagedList(List<CoffeeDto> content){
         super(content);
     }
+
+
 }
